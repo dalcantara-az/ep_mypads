1ccfd9e1af9e94c1663bb20cbb08ee76a281b6ab
Melody <melody_go@dlsu.edu.ph>
2020-04-21 17:14:23 +0800

    auto refresh when self watch

--- frontend/js/modules/group-watch.js
+++ frontend/js/modules/group-watch.js
@@ -66,6 +66,7 @@ module.exports = (function () {
         }
       }).then(function () {
         notif.success({ body: conf.LANG.GROUP.WATCH_SUCCESS });
+        model.fetch(c.computeGroups);
         if (successFn) { successFn(); }
       }, function (err) {
         return notif.error({ body: ld.result(conf.LANG, err.error) });
