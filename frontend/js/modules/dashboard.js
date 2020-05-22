83b099eb87834eecb973dae9b4884d2fcc05cbba
Melody <melody_go@dlsu.edu.ph>
2020-05-22 14:08:50 +0800

    fixed time format

--- frontend/js/modules/dashboard.js
+++ frontend/js/modules/dashboard.js
@@ -425,7 +425,7 @@ module.exports = (function () {
               var year = date.getFullYear();
               var hour = date.getHours();
               var minutes = date.getMinutes();
-              if(minutes < 9){
+              if(minutes <= 9){
                 minutes = "0"+ minutes;
               }
 
